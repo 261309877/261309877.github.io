9064c5f8cd19b16d163ecf93bf1c8525102835f6
有肉不吵 <261309877@qq.com>
2020-11-04 09:06:56 +0800

    Add files via upload
    改为随机选题

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var currentQ = new Object();
 var currentA = "";
+var random = 0;
 var allQuestions = $.extend([], true, test);
 var wrongQs = [];
 var lineNumber = 0;
 
 function initPage() {
-    showLineQ();
-    // showRandomQ()
+    // showLineQ();
+    showRandomQ()
     event();
 }
 
@@ -18,16 +19,22 @@ function showLineQ() {
 }
 
 function showRandomQ() {
-    var random = parseInt(Math.random() * allQuestions.length);
-    currentQ = allQuestions[random];
-    currentA = currentQ["answer"]
-    $(".question-box").html(currentQ["question"]);
+    if (allQuestions.length) {
+        random = parseInt(Math.random() * allQuestions.length);
+        currentQ = allQuestions[random];
+        currentA = currentQ["answer"];
+        $(".question-box").html(currentQ["question"]);
+    } else {
+        $(".tips").html("答题结束");
+    }
+
 }
 
 function event() {
     $(document).on("click", ".answer-box label", function () {
         var seletValue = $(this).find("input").val();
         if (seletValue == currentA) {
+            allQuestions.splice(random, 1);
             $(".tips").html("回答正确！");
         } else {
             wrongQs.push(currentQ);
@@ -35,14 +42,20 @@ function event() {
             $(".tips").html("正确答案：" + currentA);
             console.log(allQuestions);
         }
-        setTimeout(function () {
-            showLineQ();
-            // showRandomQ();
-            $(".tips").html("");
-            $.each($(".answer-box input[type='radio']"), function (pro, item) {
-                $(item).removeAttr('checked');
-            })
-        }, 1500)
+        if(allQuestions.length){
+            setTimeout(function () {
+                // showLineQ();
+                showRandomQ();
+                $(".tips").html("");
+                $.each($(".answer-box input[type='radio']"), function (pro, item) {
+                    $(item).removeAttr('checked');
+                })
+            }, 1500)
+        }else{
+            $(".answer-box").remove();
+            $(".question-box").html("");
+            $(".tips").html("答题结束, 共计错误"+wrongQs.length+"题");
+        }
     })
 }
 
