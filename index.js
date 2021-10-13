237eec0b807fa0a261640159ef1ce486effdb905
有肉不吵 <261309877@qq.com>
2021-10-13 14:59:41 +0800

    second

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ var allQuestions = $.extend([], true, test);
 var wrongQs = [];
 var lineNumber = 0;
 
+var existIdCards= [
+    "ctsi@123"
+]
+
 function initPage() {
     // showLineQ();
     showRandomQ()
-    event();
 }
 
 function showLineQ() {
@@ -23,15 +26,23 @@ function showRandomQ() {
         random = parseInt(Math.random() * allQuestions.length);
         currentQ = allQuestions[random];
         currentA = currentQ["answer"];
+        currentQType = currentQ["type"]
         $(".question-box").html(currentQ["question"]);
+        if (currentQType && currentQType == "checkbox") {
+            $(".radio-answer-box").hide();
+            $(".checkbox-answer-box").show()
+        } else {
+            $(".radio-answer-box").show();
+            $(".checkbox-answer-box").hide()
+        }
     } else {
         $(".tips").html("答题结束");
     }
 
 }
 
-function event() {
-    $(document).on("click", ".answer-box label", function () {
+function addEvent() {
+    $(document).on("click", ".radio-answer-box label", function () {
         var seletValue = $(this).find("input").val();
         if (seletValue == currentA) {
             allQuestions.splice(random, 1);
@@ -42,21 +53,63 @@ function event() {
             $(".tips").html("正确答案：" + currentA);
             console.log(allQuestions);
         }
-        if(allQuestions.length){
+        if (allQuestions.length) {
             setTimeout(function () {
                 // showLineQ();
                 showRandomQ();
                 $(".tips").html("");
-                $.each($(".answer-box input[type='radio']"), function (pro, item) {
+                $.each($(".radio-answer-box input[type='radio']"), function (pro, item) {
                     $(item).removeAttr('checked');
                 })
             }, 1500)
-        }else{
+        } else {
             $(".answer-box").remove();
             $(".question-box").html("");
-            $(".tips").html("答题结束, 共计错误"+wrongQs.length+"题");
+            $(".tips").html("答题结束, 共计错误" + wrongQs.length + "题");
+        }
+    })
+
+    $(document).on("click", ".checkbox-answer-box .make-sure-btn", function () {
+        var checkboxValue = [];
+        var seletOptions = $(".checkbox-answer-box input[type='checkbox']:checked");
+        seletOptions.each(function () {
+            let tempVal = $(this).val();
+            checkboxValue.push(tempVal);
+        })
+
+        if (JSON.stringify(checkboxValue) == JSON.stringify(currentA)) {
+            allQuestions.splice(random, 1);
+            $(".tips").html("回答正确！");
+        } else {
+            wrongQs.push(currentQ);
+            allQuestions.push(currentQ);
+            $(".tips").html("正确答案：" + currentA.toString());
+            console.log(allQuestions);
+        }
+        if (allQuestions.length) {
+            setTimeout(function () {
+                // showLineQ();
+                showRandomQ();
+                $(".tips").html("");
+                $.each($(".checkbox-answer-box input[type='checkbox']"), function (pro, item) {
+                    $(item).removeAttr('checked');
+                })
+            }, 1500)
+        } else {
+            $(".answer-box").remove();
+            $(".question-box").html("");
+            $(".tips").html("答题结束, 共计错误" + wrongQs.length + "题");
+        }
+    })
+
+    $(document).on("click", ".check-box .check-btn", function(){
+        let checkVal = $(".id-card").val();
+        if(existIdCards.includes(checkVal)){
+            $(".question-box-content").show();
+            $(".check-box").hide();
+            initPage();
         }
     })
 }
 
-initPage();
+addEvent();
